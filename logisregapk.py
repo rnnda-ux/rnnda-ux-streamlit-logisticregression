8d34a00c78bb29dda098ce2b429a2c826a8a701b
rnnda-ux <rnandajr758@gmail.com>
2025-01-09 07:52:48 +0700

    Add files via upload

---
+++ logisregapk.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load('D:/Machine Learning/akhir.py/Supervised/logistic_regression_model.pkl')
+scaler = joblib.load('D:/Machine Learning/akhir.py/Supervised/scaler.pkl')
+le = joblib.load('D:/Machine Learning/akhir.py/Supervised/label_encoder.pkl')
+
+st.title("Prediksi Spesies Ikan")
+
+# Input data oleh pengguna
+length = st.number_input("Panjang Ikan (length)", min_value=0.0)
+weight = st.number_input("Berat Ikan (weight)", min_value=0.0)
+w_l_ratio = st.number_input("Rasio Berat/Panjang (w_l_ratio)", min_value=0.0)
+
+# Tombol untuk melakukan prediksi
+if st.button("Prediksi"):
+    # Proses data input pengguna
+    input_data = np.array([[length, weight, w_l_ratio]])
+
+    # Standarisasi data input
+    scaled_data = scaler.transform(input_data)
+
+    # Prediksi spesies ikan
+    prediction = model.predict(scaled_data)
+
+    # Convert hasil prediksi ke nama spesies
+    predicted_species = le.inverse_transform(prediction)
+
+    # Tampilkan hasil prediksi
+    st.write(f"Prediksi Spesies Ikan: {predicted_species[0]}")
